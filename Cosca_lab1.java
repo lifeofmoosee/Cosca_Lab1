286f20d3b4e6b315d7091abdf9e6062dfd6e9530
lifeofmoosee <coscaalex@gmail.com>
2024-09-01 16:14:26 +0800

    Update Cosca_lab1.java

--- Cosca_lab1.java
+++ Cosca_lab1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HalfDiamondPattern {
+public class Cosca_lab1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter number: ");
